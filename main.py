2f09cfd1fead346ad6b4d44614ce3213acbcf4f0
Mykola Yevdokimov <iemnoga@gmail.com>
2024-02-28 12:05:08 +0200

    add trades_df?, depth_df

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 from binance import Client, ThreadedDepthCacheManager, ThreadedWebsocketManager
 import pandas as pd
+import mplfinance as mpf
+
 from api import APIKEY, SECRETKEY
 
 client = Client(APIKEY, SECRETKEY)
@@ -13,11 +15,17 @@ ticket_price = ticker['price']
 
 trades = client.get_historical_trades(symbol= ticker_symbol)
 
-ticker_df = pd.DataFrame(trades)
+depth = client.get_order_book(symbol= ticker_symbol)
 
-depth = client.get_order_book(symbol= 'SOLUSDT')
+depth_df = pd.DataFrame(depth)
 
-historical = client.get_historical_klines('SOLUSDT', Client.KLINE_INTERVAL_1DAY,'1 Jan 2024')  #OHLCV (Open, High, Low, Close, Volume)
+trades_df = pd.DataFrame(trades)
 
+
+historical = client.get_historical_klines('SOLUSDT', Client.KLINE_INTERVAL_1DAY,'1 Jan 2024')  
+'''
+list of OHLCV values (Open time, Open, High, Low, Close, Volume, 
+Close time, Quote asset volume, Number of trades, Taker buy base asset volume, Taker buy quote asset volume, Ignore)
+'''
 
 
